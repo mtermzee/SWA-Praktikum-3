6b8a7eac7946d5a299a27a8f416a7d936de17018
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-11-20 14:55:45 +0100

    init

--- src/main/java/de/hsos/swa/cocktail/ECB/boundry/CocktailResource.java
+++ src/main/java/de/hsos/swa/cocktail/ECB/boundry/CocktailResource.java
@@ -12,6 +12,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 import de.hsos.swa.cocktail.ECB.control.cocktail.CocktailService;
 import de.hsos.swa.cocktail.ECB.control.dto.CocktailDTO;
 import de.hsos.swa.cocktail.ECB.control.dto.IngredientDTO;
@@ -19,12 +27,16 @@ import de.hsos.swa.cocktail.ECB.control.dto.IngredientDTO;
 @Path("/cocktails")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@OpenAPIDefinition(info = @Info(title = "Cocktails", version = "1.2", description = "You can get your drinks "))
+
 public class CocktailResource {
     @Inject
     CocktailService cocktailService;
 
     @GET
     @Path("{name}")
+    @Operation(summary = "search your cocktail")
+    @APIResponses(value = @APIResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CocktailDTO.class))))
     public Response getCocktailByName(@PathParam("name") String name) {
         List<CocktailDTO> cocktails = cocktailService.getCocktailByName(name);
 
@@ -37,6 +49,8 @@ public class CocktailResource {
 
     @GET
     @Path("ingredients/{name}")
+    @Operation(summary = "search your ingredient")
+    @APIResponses(value = @APIResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = IngredientDTO.class))))
     public Response getIngredientByName(@PathParam("name") String name) {
         List<IngredientDTO> ingredients = cocktailService.getIngredientByName(name);
 
